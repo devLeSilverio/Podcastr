398bcc25b18936d2290a10e844dc8cdce1d96d4d
devLeSilverio <leticia.silverio09@gmail.com>
2021-04-23 22:43:46 -0300

    🌻I made the function convertDurationToTime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@
 //SSR --> recarrega toda vez que a cessar a home da aplicação , mas se ela na sofre alteração, nao tem porque ir na api e buscra os episodios
 //SSG --> asssim que entra na pag, a aplicacao que noa muda fica estatica  
 
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { GetStaticProps } from "next"
 import api from "../../services/api"
+import convertDurationToTimeString from '../utils/convertDurationToTimeString'
 
 type Episode = {
   id:string;
   title:string;
   members:string;
+  published_at:string;
 }
 
 type HomeProps = {
@@ -33,9 +37,22 @@ export  const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const episodes = data.map(episode =>{
+    return{
+      id:episode.id,
+      title:episode.title,
+      thumbnail:episode.thumbnail,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR} ),
+      duration:Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
+
   return {
-    props:{
-      episodes:data,
+    props: {
+      episodes,
     },
     revalidate: 60*60*8,
   }
