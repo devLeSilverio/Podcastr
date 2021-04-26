378cb71ccd6dcbe5bfdaa560d18b147a4e47df95
devLeSilverio <leticia.silverio09@gmail.com>
2021-04-26 20:55:13 -0300

    🌻 finished the aplication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
-import Header from '../../src/components/Header';
-import Player from '../../src/components/Player';
 import styles from '../../styles/app.module.scss';
 import '../../styles/globals.scss';
+import Header from '../components/Header';
+import { Player } from '../components/Player';
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
-function MyApp({Component,pageProps}){
+function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header/>
-        <Component {...pageProps} />
-      </main>
-      <Player/>
-    </div>
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
   )
 }
 
